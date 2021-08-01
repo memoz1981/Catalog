2c5d5c2a461dbe19ce01c1f2b7b3d52afc4adb77
memoz1981 <zeynalov@gmail.com>
2021-08-01 21:22:52 +0400

    Repository and InMemItemsRepository class added

---
+++ Properties/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> Get()
+        {
+            return items; 
+        }
+
+        public Item GetItems(Guid id)
+        {
+            return items.FirstOrDefault(m=>m.Id==id);
+        }
+
+    }
+}
